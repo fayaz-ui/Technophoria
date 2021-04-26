fe446569b2539b6ad12caf7e6457bc1eeb3ec11d
fayaz-ui <ansarifayaz450@gmail.com>
2021-04-27 04:44:59 +0530

    uploading directories

---
+++ javascript/index.js
@@ -0,0 +1,47 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks= document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click',()=>{
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link=>{
+    link.addEventListener('click',()=>{
+        document.body.classList.remove('nav-open');
+    })
+})
+/*
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
+
+ /* and end with this is a dummy comment */
+
+ 
+function myFunction() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+    dots.style.display = "inline";
+      btnText.innerHTML = "Read more"; 
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "Read less"; 
+      moreText.style.display = "inline";
+    }
+  }
